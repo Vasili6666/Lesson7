b3cb4c059f802d87a0b76c631fe1cf51dc219c3a
Vasili Pl <w2e3r4t51982@gmail.com>
2025-08-28 16:22:49 +0200

    working version 1.1

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,6 +6,12 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+
+        // Добавляем ведущий ноль для дней < 10
+        String formattedDay = day.length() == 1 ? "0" + day : day;
+        $(".react-datepicker__day--0" + formattedDay + ":not(.react-datepicker__day--outside-month)").click();
     }
+
 }
+
+
